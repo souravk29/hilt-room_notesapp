944b2b4e309cb3cd9479921ec3800e9a818df1f6
souravk29 <sk2661901@gmail.com>
2025-12-10 23:21:41 +0530

    updated

--- app/src/main/java/com/example/hilt_room_notesapp/room/NoteDao.kt
+++ app/src/main/java/com/example/hilt_room_notesapp/room/NoteDao.kt
@@ -1,12 +1,15 @@
 package com.example.hilt_room_notesapp.room
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 /*
 
 
 this interface contains the method to interact with the database, defining how your
-       app will read/write data to teh note table that you created earlier
+       app will read/write data to the "note" table that you created earlier
 
 
  */
@@ -15,4 +18,26 @@ this interface contains the method to interact with the database, defining how y
 @Dao
 interface NoteDao {
 
+    @Insert
+    suspend fun insert(note: Note)                                          // tells room to add a new note to the database
+
+    @Query("SELECT * FROM notes ORDER BY timestamp DESC")
+    fun getAllNotes(): Flow<List<Note>>                                       // fetching 
+
 }
+
+
+
+
+
+
+
+
+/*
+
+ A suspending function is a special type of function in Kotlin that can be paused and resumed at a later time.
+ This is the foundational element of Kotlin Coroutines. Suspending functions are designed to perform long-running tasks
+ (like database operations, network calls, or intensive calculations) without blocking the main thread (e.g., the UI
+ thread in an Android application).
+
+*/
